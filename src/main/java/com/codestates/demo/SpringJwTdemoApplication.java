c571ea6383c8d6768b8e506a97ada95503bdee63
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2022-07-27 13:00:21 +0900

    Initialize Project

---
+++ src/main/java/com/codestates/demo/SpringJwTdemoApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwTdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwTdemoApplication.class, args);
+	}
+
+}
